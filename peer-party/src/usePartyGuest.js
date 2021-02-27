bfaa95c1c356ef40016891d6ea69550320a5358e
James O'Toole <james.o'toole@bbc.co.uk>
2021-02-27 20:16:41 +0000

    Refactor

--- peer-party/src/usePartyGuest.js
+++ peer-party/src/usePartyGuest.js
@@ -2,6 +2,63 @@ import { useEffect, useRef, useState } from "react";
 import Peer from "peerjs";
 import cloneDeep from "lodash/cloneDeep";
 
+const constructPeer = ({ peer, id }) => {
+  peer.current = new Peer(id);
+};
+
+const destructPeer = ({ peer }) => {
+  peer.current.destroy();
+  peer.current = null;
+}
+
+const connect = ({ roomId, peer, conn }) => {
+  conn.current = peer.current.connect(roomId);
+}
+
+const increaseLogSize = (logSize, value) => logSize.current += value;
+
+const ack = ({ conn, logSize }) =>  
+  conn.current.send({ index: logSize.current })
+
+const sync = ({ state, setCache, roomId, game, events }) => {
+
+  const reduceMove = (o, { move, args, connectionId }) => {
+    const reducer = connectionId === roomId ? game.hostMoves[move] : game.guestMoves[move];
+    try {
+      return reducer({ state: o, args, connectionId });
+    } catch (e) {
+      console.error(e);
+      return o;
+    }
+  }
+
+  state.current = events.reduce(reduceMove, cloneDeep(state.current));
+  setCache(() => cloneDeep(state.current));
+}
+
+const emit = ({ conn, logSize, move, args }) => 
+  conn.current.send({ index: logSize.current, move, args })
+
+const preempt = ({ game, move, args, setCache, peer }) => {
+  setCache(c => {
+    try {
+      return game.guestMoves[move]({ state: c, args, connectionId: peer.current.id })
+    } catch (e) {
+      console.error(e);
+      return c;
+    }
+  })
+}
+
+const constructMovesHandler = ({ conn, moves, setCache, peer, game, logSize }) => {
+  moves.current = new Proxy({}, {
+    get: (_, move) => (args) => {
+      preempt({ game, move, args, setCache, peer })
+      emit({ conn, move, args, logSize })
+    }
+  });
+}
+
 const usePartyGuest = ({ id, roomId, game }) => {
 
   const peer = useRef();
@@ -16,50 +73,22 @@ const usePartyGuest = ({ id, roomId, game }) => {
 
     if (!id || !game) return undefined;
 
-    peer.current = new Peer(id);
+    constructPeer({ peer, id })
 
     peer.current.on("open", () => {
-      conn.current = peer.current.connect(roomId);
 
+      connect({ conn, peer, roomId });
+      constructMovesHandler({ conn, moves, setCache, peer, game, logSize })
 
       conn.current.on("data", (events) => {
-
-        logSize.current += events.length;
-
-        const reduceMove = (o, { move, args, connectionId }) => {
-          const reducer = connectionId === roomId ? game.hostMoves[move] : game.guestMoves[move];
-          try {
-            return reducer({ state: o, args, connectionId });
-          } catch (e) {
-            console.error(e);
-            return o;
-          }
-        }
-        state.current = events.reduce(reduceMove, cloneDeep(state.current));
-        setCache(() => cloneDeep(state.current));
-
-        conn.current.send({ index: logSize.current })
+        increaseLogSize(logSize, events.length);
+        sync({ state, setCache, roomId, game, events })
+        ack({ conn, logSize })
       });
 
-      moves.current = new Proxy({}, {
-        get: (_, move) => (args) => {
-          setCache(c => {
-            try {
-              return game.guestMoves[move]({ state: c, args, connectionId: peer.current.id })
-            } catch (e) {
-              console.error(e);
-              return c;
-            }
-          })
-          conn.current.send({ index: logSize.current, move, args })
-        }
-      });
     });
 
-    return () => {
-      peer.current.destroy();
-      peer.current = null;
-    }
+    return () => destructPeer({ peer })
 
   }, [id, roomId, game])
 
--- peer-party/src/usePartyHost.js
+++ peer-party/src/usePartyHost.js
@@ -11,93 +11,118 @@ const validateEvent = (event, validMoves) => (
 
 const hasHostMove = (game, move) => Boolean(game.hostMoves[move])
 
-const usePartyHost = ({ roomId, game }) => {
+const constructMovesHandler = ({ moves, game, setState, roomId, setEventLog }) => {
+  moves.current = new Proxy({}, {
+    get: (_, move) => (
+      hasHostMove(game, move)
+        ? (args) => logEvent({ setEventLog, event: { move, args }, connectionId: roomId })
+        : Reflect.get(...arguments)
+    )
+  });
+}
 
-  const moves = useRef();
+const constructPeer = ({ peer, roomId }) => {
+  peer.current = new Peer(roomId);
+}
 
+const destructPeer = ({ peer }) => {
+  peer.current.destroy();
+  peer.current = null;
+}
+
+const appendConnection = ({ setConnections, conn }) => {
+  setConnections((conns) => [...conns, conn]);
+}
+
+const updateLogSizeMap = ({ conn, connectionLogSizeMap, size = 0 }) => {
+  connectionLogSizeMap.current[conn.peer] = size
+}
+
+const logEvent = ({ setEventLog, event, connectionId }) => {
+  setEventLog(events => [...events, { ...event, connectionId }]);
+}
+
+const updateState = ({ roomId, setState, game, events }) => {
+  setState(state => {
+    try {
+      return events.reduce(
+        (o, event) => {
+          const moves = (event.connectionId === roomId ? game.hostMoves : game.guestMoves);
+          return moves[event.move]({ state: o, connectionId: event.connectionId, args: event.args });
+        },
+        state
+      );
+    } catch (e) {
+      console.error(e);
+      return state;
+    }
+  })
+}
+
+const useConnections = ({ game, roomId, setState, eventLog, setEventLog }) => {
   const peer = useRef();
-  const eventLog = useRef([]);
-
+  const [connections, setConnections] = useState([]);
   const connectionLogSizeMap = useRef({});
 
-  const [state, setState] = useState({});
-  const [connections, setConnections] = useState([]);
-
   useEffect(() => {
-
-    if (!game) return;
-
-    moves.current = new Proxy({}, {
-      get: (_, move) => (
-        hasHostMove(game, move)
-        ? ((args) => {
-            setState(o => game.hostMoves[move]({
-              state: o,
-              connectionId: roomId,
-              args
-            }))
-            eventLog.current.push({ move, args, connectionId: roomId })
-          })
-        : Reflect.get(...arguments)
-      )
-    });
-
-    let timeouts = {};
-    peer.current = new Peer(roomId);
+    constructPeer({ peer, roomId });
     peer.current.on("open", () => {
       peer.current.on("connection", (conn) => {
 
-        setConnections((conns) => [...conns, conn.peer]);
-        connectionLogSizeMap.current[conn.peer] = 0
+        appendConnection({ setConnections, conn });
+        updateLogSizeMap({ conn, connectionLogSizeMap });
 
         conn.on("data", ({ index, ...event }) => {
-
-          // Update log sizes mapped to connection ids
           if (!isInteger(index)) return;
-          connectionLogSizeMap.current[conn.peer] = index
+          updateLogSizeMap({ conn, connectionLogSizeMap, size: index });
 
           if (!validateEvent(event, Object.keys(game.guestMoves))) return;
 
-          // Add to event log
-          eventLog.current.push({ ...event, connectionId: conn.peer });
+          logEvent({ setEventLog, event, connectionId: conn.peer })
 
-          // Apply events to state
-          setState(o => {
-            try {
-              return game.guestMoves[event.move]({ state: o, connectionId: conn.peer, args: event.args })
-            } catch (e) {
-              console.error(e);
-              return o;
-            }
-          })
         })
-
-        const ticker = (tick) => () => {
-          tick()
-          timeouts[conn.peer] = setTimeout(ticker(tick), 500)
-        }
-
-        ticker(() => {
-          const numSent = connectionLogSizeMap.current[conn.peer];
-          if (eventLog.current.length > numSent) {
-            const evts = eventLog.current.slice(numSent);
-            conn.send(evts);
-          }
-        })()
-
-
       })
     })
-
     return () => {
-      Object.values(timeouts).forEach(timeout => clearTimeout(timeout));
-      peer.current.destroy();
-      peer.current = null;
+      destructPeer({ peer });
     }
-  }, [roomId, game])
+  }, [])
 
-    
-  return { state, moves: moves.current, connections }
+  return { eventLog, connections, connectionLogSizeMap }
+}
+
+const usePartyHost = ({ roomId, game }) => {
+  const moves = useRef();
+  const logSize = useRef(0);
+  const [state, setState] = useState({});
+  const [eventLog, setEventLog] = useState([]);
+  const { connections, connectionLogSizeMap } = useConnections({ game, roomId, setState, eventLog, setEventLog })
+
+  useEffect(() => {
+    if (!game) return;
+    constructMovesHandler({ moves, game, setState, roomId, setEventLog });
+  }, [roomId, game]);
+
+  useEffect(() => {
+    for (const connection of connections) {
+      const numSent = connectionLogSizeMap.current[connection.peer];
+      if (eventLog.length > numSent) {
+        const events = eventLog.slice(numSent);
+        connection.send(events);
+      }
+    }
+  }, [connections, connectionLogSizeMap, eventLog]);
+
+  useEffect(() => {
+    const events = eventLog.slice(logSize.current);
+    if (!roomId || !game || !events.length) return;
+    updateState({ roomId, setState, game, events });
+    logSize.current = eventLog.length;
+  }, [roomId, game, eventLog, logSize])
+
+  console.log("state", state);
+
+  return { state, moves: moves.current, connections: connections.map(({ peer }) => peer) }
 }
 
 export default usePartyHost;
