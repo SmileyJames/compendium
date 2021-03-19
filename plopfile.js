1f8b91e778d3e89918bce13a871bfa7b6a32b34b
James O'Toole <james.o'toole@bbc.co.uk>
2021-03-19 09:18:56 +0000

    Add a template for all the code and condig needed for a new game - run Usage Error: Couldn't find a script named "plop".
    $ yarn run [--inspect] [--inspect-brk] <scriptName> ... to start making a game

--- plopfile.js
+++ plopfile.js
@@ -4,31 +4,95 @@ module.exports = (plop) => {
     prompts: [
       {
         type: 'input',
-        name: 'kebab-case',
-        message: 'Please enter the name of your game. (lower-cased-kebab-sticked)'
+        name: 'packageName',
+        message: 'Please enter the package name of your game. (lower-cased-kebab-sticked)'
       },
       {
         type: 'input',
-        name: 'CamelCasedName',
-        message: 'Please enter the name of your game. (CamelCased)'
+        name: 'camelCasedName',
+        message: 'Please enter the component name of your game. (CamelCased)'
+      },
+      {
+        type: 'input',
+        name: 'verboseName',
+        message: 'Please enter the verbose name of your game. (Like a Title)'
       }
     ],
     actions: [
+      // Stories
       {
         type: 'add',
-        path: 'compendium/{{kebab-case}}/src/index.js',
-        templateFile: 'plop-templates/index.js.hbs'
+        path: 'app/src/stories/{{ camelCasedName }}/Host.stories.js',
+        templateFile: 'templates/HostStory.js.hbs'
       },
       {
         type: 'add',
-        path: 'package.json',
-        templateFile: 'plop-templates/package.json.hbs'
+        path: 'app/src/stories/{{ camelCasedName }}/Guest.stories.js',
+        templateFile: 'templates/GuestStory.js.hbs'
+      },
+      // Root game package
+      {
+        type: 'add',
+        path: 'compendium/{{ packageName }}/README.md',
+        templateFile: 'templates/game/README.md.hbs'
       },
       {
         type: 'add',
-        path: 'package.json',
-        templateFile: 'plop-templates/package.json.hbs'
-      }
+        path: 'compendium/{{ packageName }}/index.js',
+        templateFile: 'templates/game/index.js.hbs'
+      },
+      {
+        type: 'add',
+        path: 'compendium/{{ packageName }}/package.json',
+        templateFile: 'templates/game/package.json.hbs'
+      },
+      // Client package
+      {
+        type: 'add',
+        path: 'compendium/{{ packageName }}/client/README.md',
+        templateFile: 'templates/game/client/README.md.hbs'
+      },
+      {
+        type: 'add',
+        path: 'compendium/{{ packageName }}/client/package.json',
+        templateFile: 'templates/game/client/package.json.hbs'
+      },
+      {
+        type: 'add',
+        path: 'compendium/{{ packageName }}/client/src/index.js',
+        templateFile: 'templates/game/client/src/index.js.hbs'
+      },
+      {
+        type: 'add',
+        path: 'compendium/{{ packageName }}/client/babel.config.json',
+        templateFile: 'templates/game/client/babel.config.json.hbs'
+      },
+      {
+        type: 'add',
+        path: 'compendium/{{ packageName }}/client/src/Host.js',
+        templateFile: 'templates/game/client/src/Host.js.hbs'
+      },
+      {
+        type: 'add',
+        path: 'compendium/{{ packageName }}/client/src/Guest.js',
+        templateFile: 'templates/game/client/src/Guest.js.hbs'
+      },
+      // Game rules package
+      {
+        type: 'add',
+        path: 'compendium/{{ packageName }}/game/README.md',
+        templateFile: 'templates/game/game/README.md.hbs'
+      },
+      {
+        type: 'add',
+        path: 'compendium/{{ packageName }}/game/package.json',
+        templateFile: 'templates/game/game/package.json.hbs'
+      },
+      {
+        type: 'add',
+        path: 'compendium/{{ packageName }}/game/src/index.js',
+        templateFile: 'templates/game/game/src/index.js.hbs'
+      },
     ]
   });
 }
