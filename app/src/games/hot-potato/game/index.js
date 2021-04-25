d4c1110fa04c1a7f08b4a51725043afd71fa2320
James O'Toole <james.o'toole@bbc.co.uk>
2021-04-25 14:35:47 +0100

    initial commit for Hot Potato

---
+++ app/src/games/hot-potato/game/index.js
@@ -0,0 +1,11 @@
+const game = {
+  guestMoves: {
+    // add moves here
+  },
+  
+  hostMoves: {
+    // add moves here
+  },
+}
+
+export default game;
---
+++ app/src/games/hot-potato/game/index.test.js
@@ -0,0 +1,11 @@
+import game from "./index"
+
+describe("Hot Potato Game", () => {
+    describe("Guest Moves", () => {
+
+    })
+
+    describe("Host Moves", () => {
+
+    })
+})
