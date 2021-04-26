3439c240c7a7c59410c3d0ff131770048b7e1c9f
James O'Toole <james.o'toole@bbc.co.uk>
2021-04-26 19:09:11 +0100

    Add hot potato's guest component

--- app/src/games/hot-potato/client/Guest.js
+++ app/src/games/hot-potato/client/Guest.js
@@ -1,16 +1,62 @@
 import React from "react";
-import { Text } from "rebass/styled-components";
+import Emoji from "components/emoji";
+import { Text, Button, Box } from "rebass/styled-components";
 
-const Guest = ({ state, roomId, moves, connectionId, connected }) => {
-  return (
+const Container = Box
+
+const Disconnected = () => (
+  <Container>
     <Text
       fontSize={[ 3, 4, 5 ]}
       fontWeight='bold'
       color='primary'
     >
-      Guest
+      Not Connected to the Host!
     </Text>
-  );
+  </Container>
+);
+
+const OpenHands = () => (
+  <Container>
+    <Emoji />
+    <Text
+      fontSize={[ 3, 4, 5 ]}
+      fontWeight='bold'
+      color='primary'
+    >
+      Phew, no hot potato! 
+    </Text>
+  </Container>
+)
+
+const Potato = ({ onPassPotato = () => {} }) => (
+  <Container>
+    <Emoji />
+    <Text
+      fontSize={[ 3, 4, 5 ]}
+      fontWeight='bold'
+      color='primary'
+    >
+      Oww! Hot potato! 
+    </Text>
+    <Button onClick={onPassPotato} onKeyPress={onPassPotato} variant='primary' mr={2}>Pass the Potato</Button>
+  </Container>
+)
+
+const Guest = ({ state, roomId, moves, connectionId, connected }) => {
+  if (!connected) {
+    <Disconnected/>
+  }
+
+  if (state.hotPotato) {
+    return (
+      <Potato onPassPotato={() => moves.passPotato()} />
+    )
+  } else {
+    return (
+      <OpenHands/>
+    )
+  }   
 };
 
 export default Guest;
