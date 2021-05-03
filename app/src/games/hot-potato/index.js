823c4a54f0a0627a536621d747a7fb4f194d3946
James O'Toole <james.o'toole@bbc.co.uk>
2021-05-03 11:44:15 +0100

    Use players component with hot potato game, later will refactor to HOC

--- app/src/games/hot-potato/index.js
+++ app/src/games/hot-potato/index.js
@@ -1,10 +1,25 @@
 import game from "./game";
 import { Host, Guest } from "./client";
 import presetTheme from '@rebass/preset'
+import { withGuestPlayers, withHostPlayers }  from "components/players/client"
+import Players  from "components/players"
 
 const name = "Hot Potato";
 const theme = presetTheme;
 
-const HotPotato = { name, theme, Host, Guest, game };
+const HotPotato = { name, theme,
+    Host: withHostPlayers({ maxPlayers: 16, minPlayers: 2 }, Host),
+    Guest: withGuestPlayers(Guest),
+    game: {
+        hostMoves: {
+            ...Players.game.hostMoves,
+            ...game.hostMoves,
+        },
+        guestMoves: {
+            ...Players.game.guestMoves,
+            ...game.guestMoves,
+        }
+    }
+};
 
 export default HotPotato;
