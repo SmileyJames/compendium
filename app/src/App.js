1309d66293e5c67b48eeabef1ca33a2be507c9c4
James O'Toole <james.o'toole@bbc.co.uk>
2021-06-27 18:15:18 +0100

    Provide a title and remove default padding around body

--- app/src/App.js
+++ app/src/App.js
@@ -1,6 +1,17 @@
 import Router from "./Router";
+import { createGlobalStyle } from "styled-components"
+
+const NormaliseStyles = createGlobalStyle`
+  body, html, #root {
+    margin: 0;
+    height: 100%;
+  }
+`
 
 const App = () =>
-  <Router/>
+  <>
+    <Router/>
+    <NormaliseStyles/>
+  </>
 
 export default App;
