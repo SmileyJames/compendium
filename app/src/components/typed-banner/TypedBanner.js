f6ffbd8868cd98ff30d1e960047ffea4f39892fd
James O'Toole <james.o'toole@bbc.co.uk>
2021-07-17 19:51:05 +0100

    Lazily load react-typed to improve performance

--- app/src/components/typed-banner/TypedBanner.js
+++ app/src/components/typed-banner/TypedBanner.js
@@ -1,18 +1,22 @@
-import ReactTyped from "react-typed";
+import { Suspense, lazy } from "react";
 import { Heading, Flex } from "rebass/styled-components";
+
+const ReactTyped = lazy(() => import("react-typed"));
 
 const TypedBanner = ({ typedSentences, stopped = false }) => (
   <Flex px="20%" py={5} my={4} backgroundColor="secondary">
     <Heading color="muted" sx={{ userSelect: "none" }}>
-      <ReactTyped
-        strings={typedSentences}
-        typeSpeed={50}
-        backSpeed={50}
-        backDelay={1}
-        loop
-        smartBackspace
-        stopped={stopped}
-      />
+      <Suspense fallback="Loading...">
+        <ReactTyped
+          strings={typedSentences}
+          typeSpeed={50}
+          backSpeed={50}
+          backDelay={1}
+          loop
+          smartBackspace
+          stopped={stopped}
+        />
+      </Suspense>
     </Heading>
   </Flex>
 );
